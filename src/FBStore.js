7adc7f7c759c2667508a064ef022699802c05124
Cy Bae <cybae0804@gmail.com>
2018-12-04 10:08:58 -0800

    Changing fb import

--- src/FBStore.js
+++ src/FBStore.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 import { observable, action, computed } from "mobx";
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 class FBStore {
     constructor () {
