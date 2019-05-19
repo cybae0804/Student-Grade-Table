142cccea20933540fe95019379138469a3f983b1
Cy Bae <cybae0804@gmail.com>
2019-05-18 19:05:42 -0700

    App.test.js now passes

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
+import Firebase from '../stores/Firebase.js';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider Firebase={new Firebase()}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
